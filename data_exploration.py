dd8e38c9d205557d358abb405c59d1abca4f7822
Bikash12 <bikash8181@gmail.com>
2019-03-02 13:38:02 +0530

    commiting data exploration file

--- data_exploration.py
+++ data_exploration.py
@@ -27,3 +27,28 @@ plt.plot(range(1,num_classes),unique.values[1:],'-k')
 plt.xlabel('labels converted to arbitray int')
 plt.ylabel('occurences')
 plt.show()
+
+#the most common aspect ratio
+H = []
+W = []
+count = 0
+folder = 'C:/Users/Bikash/Downloads/train/'
+t0 = time.time()
+for fname in os.listdir(folder):
+    if fname[-3:] == 'jpg':
+        img = skimage.io.imread(folder+fname)
+        H.append(img.shape[0])
+        W.append(img.shape[1])
+        count += 1
+#     if count == 100:
+#         break
+
+H = np.array(H,dtype='float')
+W = np.array(W,dtype='float')
+A = H/W
+unique, ret_counts = np.unique(A,return_counts=True)
+
+plt.figure()
+plt.plot(unique,ret_counts)
+plt.xlabel('Aspect ratio')
+plt.ylabel('Occurrences')
