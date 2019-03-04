cbce85c44771e2d1a1b0d6c6be6c181bc2784779
Bikash12 <bikash8181@gmail.com>
2019-03-05 00:50:54 +0530

    commiting duplicate images file

--- duplicate_images.py
+++ duplicate_images.py
@@ -1,18 +1,138 @@
-import matplotlib.pyplot as plt
+from pandas import read_csv
+
+tagged = dict([(p, w) for _, p, w in read_csv('C:/Users/Bikash/Downloads/data/train.csv').to_records()])
+
+join = list(tagged.keys())
+
+len(tagged), len(join), list(tagged.items())[:5]
+
+# Determise the size of each image
+from os.path import isfile
+from PIL import Image as pil_image
+from tqdm import tqdm_notebook
+
+
+def expand_path(p):
+    if isfile('C:/Users/Bikash/Downloads/data/train/' + p): return 'C:/Users/Bikash/Downloads/data/train/' + p
+    if isfile('C:/Users/Bikash/Downloads/data/test/' + p): return 'C:/Users/Bikash/Downloads/data/test/' + p
+    return p
+
+
+p2size = {}
+for p in tqdm_notebook(join):
+    size = pil_image.open(expand_path(p)).size
+    p2size[p] = size
+len(p2size), list(p2size.items())[:5]
+
+# Read or generate p2h, a dictionary of image name to image id (picture to hash)
+import pickle
 import numpy as np
-import pandas
-import PIL
-import imagehash
-import time
+from imagehash import phash
+from math import sqrt
 
-df_train = pandas.read_csv('C:/Users/Bikash/Downloads/data/train.csv')
 
-# Create a new column with the hash value for the image
-t0=time.time()
-img_hash = df_train.Image.apply(lambda x: imagehash.phash(PIL.Image.open('C:/Users/Bikash/Downloads/data/train/'+x)))
-df_train['Hash_val'] = img_hash
-t1=time.time()
-t2=t1-t0
+# Two phash values are considered duplicate if, for all associated image pairs:
+# 1) They have the same mode and size;
+# 2) After normalizing the pixel to zero mean and variance 1.0, the mean square error does not exceed 0.1
+def match(h1, h2):
+    for p1 in h2ps[h1]:
+        for p2 in h2ps[h2]:
+            i1 = pil_image.open(expand_path(p1))
+            i2 = pil_image.open(expand_path(p2))
+            if i1.mode != i2.mode or i1.size != i2.size: return False
+            a1 = np.array(i1)
+            a1 = a1 - a1.mean()
+            a1 = a1 / sqrt((a1 ** 2).mean())
+            a2 = np.array(i2)
+            a2 = a2 - a2.mean()
+            a2 = a2 / sqrt((a2 ** 2).mean())
+            a = ((a1 - a2) ** 2).mean()
+            if a > 0.1: return False
+    return True
 
-num_duplicates = (df_train.Hash_val.value_counts() > 1).sum()
-num_duplicates
+
+if isfile('C:/Users/Bikash/Downloads/data/p2h.pickle'):
+    with open('C:/Users/Bikash/Downloads/data/p2h.pickle', 'rb') as f:
+        p2h = pickle.load(f)
+
+else:
+    # Compute phash for each image in the training and test set.
+    p2h = {}
+    for p in tqdm_notebook(join):
+        img = pil_image.open(expand_path(p))
+        h = phash(img)
+        p2h[p] = h
+
+    # Find all images associated with a given phash value.
+    h2ps = {}
+    for p, h in p2h.items():
+        if h not in h2ps: h2ps[h] = []
+        if p not in h2ps[h]: h2ps[h].append(p)
+
+    # Find all distinct phash values
+    hs = list(h2ps.keys())
+
+    # If the images are close enough, associate the two phash values (this is the slow part: n^2 algorithm)
+    h2h = {}
+    for i, h1 in enumerate(tqdm_notebook(hs)):
+        for h2 in hs[:i]:
+            if h1 - h2 <= 6 and match(h1, h2):
+                s1 = str(h1)
+                s2 = str(h2)
+                if s1 < s2: s1, s2 = s2, s1
+                h2h[s1] = s2
+
+    # Group together images with equivalent phash, and replace by string format of phash (faster and more readable)
+    for p, h in p2h.items():
+        h = str(h)
+        if h in h2h: h = h2h[h]
+        p2h[p] = h
+
+len(p2h), list(p2h.items())[:5]
+
+# For each image id, determine the list of pictures
+h2ps = {}
+for p, h in p2h.items():
+    if h not in h2ps: h2ps[h] = []
+    if p not in h2ps[h]: h2ps[h].append(p)
+
+len(h2ps), list(h2ps.items())[:5]
+
+# Show an example of a duplicate image (from training of test set)
+import matplotlib.pyplot as plt
+
+
+def show_whale(imgs, per_row=2):
+    n = len(imgs)
+    rows = (n + per_row - 1) // per_row
+    cols = min(per_row, n)
+    fig, axes = plt.subplots(rows, cols, figsize=(24 // per_row * cols, 24 // per_row * rows))
+    for ax in axes.flatten(): ax.axis('off')
+    for i, (img, ax) in enumerate(zip(imgs, axes.flatten())): ax.imshow(img.convert('RGB'))
+
+
+for h, ps in h2ps.items():
+    if len(ps) > 2:
+        print('Images:', ps)
+        imgs = [pil_image.open(expand_path(p)) for p in ps]
+        show_whale(imgs, per_row=len(ps))
+        break
+
+
+# For each images id, select the prefered image
+def prefer(ps):
+    if len(ps) == 1: return ps[0]
+    best_p = ps[0]
+    best_s = p2size[best_p]
+    for i in range(1, len(ps)):
+        p = ps[i]
+        s = p2size[p]
+        if s[0] * s[1] > best_s[0] * best_s[1]:  # Select the image with highest resolution
+            best_p = p
+            best_s = s
+    return best_p
+
+
+h2p = {}
+for h, ps in h2ps.items(): h2p[h] = prefer(ps)
+len(h2p), list(h2p.items())[:5]
